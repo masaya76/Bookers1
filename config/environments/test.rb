7b0d46e9d73d660ae1dc249a568f3aeb391a436d
matyaya76 <matyaya76@gmail.com>
2021-08-29 13:17:57 +0000

    1-2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :silence
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
